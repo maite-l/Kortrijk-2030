856ad9b8249d3ca57a53734e7f4c1e1f76f40cb2
maite.l <maite.lejeune@student.howest.be>
2023-06-06 18:15:45 +0200

    craft structure + listing img and pdf on frontend

--- client/src/submissions.js
+++ client/src/submissions.js
@@ -3,15 +3,35 @@ import { graphQLRequest } from "./util/graphql";
 export async function getSubmissions() {
     console.log("getSubmissions");
     const graphqlQuery = `
-    query getSubmissions {
-        submissionsEntries {
-            ... on submissions_default_Entry {
-                id
-                title
-            }
+    query MyQuery {
+    submissionsEntries {
+        ... on submissions_mixedSubmission_Entry {
+        id
+        title
+        text
+        image {
+            path
         }
+        }
+    }
     }`;
     const submissions = (await graphQLRequest(graphqlQuery)).data.submissionsEntries;
     console.log(submissions);
     return submissions;
+}
+
+export async function getMagazines() {
+    const graphqlQuery = `
+    query MyQuery {
+        magazinesEntries {
+            ... on magazines_default_Entry {
+            id
+            magazine {
+                path
+            }
+            }
+        }
+    }`;
+    const magazines = (await graphQLRequest(graphqlQuery)).data.magazinesEntries;
+    return magazines;
 }
