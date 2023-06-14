a45a39ad3563a50fe2a0997bec2a6b319037b97c
maite.l <maite.lejeune@student.howest.be>
2023-06-14 22:29:52 +0200

    simple rolling cards animation

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -21,6 +21,7 @@ import {
 import { getCurrentIssue } from "../magazines";
 
 import CustomButton from "../components/CustomButton";
+import RollingCards from "../components/RollingCards";
 
 import insta1 from "../assets/img/insta/insta1.png";
 import insta2 from "../assets/img/insta/insta2.png";
@@ -32,6 +33,7 @@ import insta7 from "../assets/img/insta/insta7.png";
 import insta8 from "../assets/img/insta/insta8.png";
 
 import tiktok from "../assets/img/tiktok.png";
+
 
 let date;
 let number;
@@ -260,13 +262,8 @@ export default function Home() {
                         </svg>
                     </a>
                 </div>
-                <div className="rolling-cards">
-                    {allMagazineSections.map((section, index) => (
-                        <RollingCard key={index} magazineSection={section} />
-                    ))}
-                </div>
+                <RollingCards allMagazineSections={allMagazineSections} />
             </div>
-
 
 
 
@@ -486,23 +483,7 @@ export function FeaturedSubmissions({ featuredSubmissions, imgURL }) {
     );
 }
 
-export function RollingCard({ magazineSection }) {
 
-    return (
-        <a href="" className="rolling-card">
-            <div>
-                <p className="rolling-card__title">{magazineSection.title}.</p>
-                <p className="rolling-card__prompt">{magazineSection.text}</p>
-            </div>
-            <div className="rolling-card__submit">
-                <p>Submit here</p>
-                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.11612 12.1161C0.627961 12.6043 0.627961 13.3957 1.11612 13.8839C1.60427 14.372 2.39573 14.372 2.88388 13.8839L1.11612 12.1161ZM14.25 2C14.25 1.30964 13.6904 0.749999 13 0.749999H1.75C1.05965 0.749999 0.500001 1.30964 0.500001 2C0.500001 2.69036 1.05965 3.25 1.75 3.25H11.75V13.25C11.75 13.9404 12.3096 14.5 13 14.5C13.6904 14.5 14.25 13.9404 14.25 13.25V2ZM2.88388 13.8839L13.8839 2.88388L12.1161 1.11612L1.11612 12.1161L2.88388 13.8839Z" fill="#030027" />
-                </svg>
-            </div>
-        </a>
-    );
-}
 
 
 export function ProgressBar({ date, progressBarPercentage }) {
