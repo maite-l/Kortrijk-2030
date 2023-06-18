b175d636927cbb84068c6b67db8c1ee46a352828
maite.l <maite.lejeune@student.howest.be>
2023-06-18 21:37:38 +0200

    attempt at making loading homepage shorter

--- client/src/util/pdfToImgSrc.js
+++ client/src/util/pdfToImgSrc.js
@@ -3,6 +3,7 @@ import * as pdfjsLib from 'pdfjs-dist';
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.7.107/pdf.worker.min.js';
 
 export default async function pdfToImgSrc(pdfDocument, allPages, amountOfPages, startPage) {
+    console.log(pdfDocument, allPages, amountOfPages, startPage);
     return pdfjsLib.getDocument(pdfDocument).promise.then((pdf) => {
         let totalPages = pdf.numPages;
 
