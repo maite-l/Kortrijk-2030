872afaa3508b478cc1b929e4e70a7acbeae4f63b
maite.l <maite.lejeune@student.howest.be>
2023-06-20 04:12:44 +0200

    added mobile nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import Modal from 'react-modal';
 import "../css/navbar.css";
 import CustomButton from "./CustomButton";
 
@@ -22,11 +23,21 @@ export default function NavBar({ loggedIn }) {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedMagazine, setSelectedMagazine] = useState(null);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
 
     return (
         <>
             {isMobile ? (
-                <nav>
+                <nav className="desktop-nav">
                     <ul>
                         <li className="logo-link">
                             <NavLink to="/">klinkt.</NavLink>
@@ -68,7 +79,97 @@ export default function NavBar({ loggedIn }) {
                     </ul>
                 </nav>
             ) : (
-                null
+                <>
+                    <div className="hamburger-icon" onClick={openModal}>
+                        <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect opacity="0.95" width="54" height="54" rx="27" fill="#030027" />
+                            <rect x="15.5" y="17.5" width="22" height="1" rx="0.5" stroke="white" />
+                            <rect x="15.5" y="26.5" width="22" height="1" rx="0.5" stroke="white" />
+                            <rect x="15.5" y="35.5" width="22" height="1" rx="0.5" stroke="white" />
+                        </svg>
+                    </div>
+                    <Modal
+                        isOpen={isOpen}
+                        onRequestClose={closeModal}
+                        contentLabel="Modal"
+                        style={{
+                            overlay: {
+                                position: 'fixed',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                backgroundColor: '#030027',
+                                zIndex: 1000,
+                            },
+                            content: {
+                                position: 'fixed',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                background: 'transparent',
+                                overflow: 'visible',
+                                WebkitOverflowScrolling: 'touch',
+                                outline: 'white 2px solid',
+                                border: 'none',
+                                borderRadius: '0',
+                                width: '100vw',
+                                height: '100vh',
+                                margin: 'auto',
+                                padding: '0',
+                                display: 'grid',
+                                placeItems: 'center',
+                                color: 'white',
+                            }
+                        }}
+                    >
+                        <div className="mobile-nav__container">
+                            <div onClick={closeModal} className="mobile-nav__close">X</div>
+                            <nav>
+                                <ul>
+                                    <li className="logo-link" onClick={closeModal}>
+                                        <NavLink to="/">klinkt.</NavLink>
+                                    </li>
+                                    <div>
+                                        <li onClick={closeModal}>
+                                            <NavLink to="/archive">Archive</NavLink>
+                                        </li>
+                                        {loggedIn ? (
+                                            <li onClick={closeModal}>
+                                                <NavLink to="/my-submissions">My Submissions</NavLink>
+                                            </li>
+                                        ) : (null)}
+                                        <li onClick={closeModal}>
+                                            <NavLink to="/submit">Submit your input</NavLink>
+                                        </li>
+                                        <li onClick={closeModal}>
+                                            <NavLink
+                                                to={loggedIn ? "/my-account" : "/login"}
+                                                className="login-link">
+                                                {loggedIn ? (
+                                                    'My Account'
+                                                ) : 'Log in'}
+                                                <svg
+                                                    width="32"
+                                                    height="32"
+                                                    viewBox="0 0 32 32"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        d="M16 0C7.168 0 0 7.168 0 16C0 24.832 7.168 32 16 32C24.832 32 32 24.832 32 16C32 7.168 24.832 0 16 0ZM16 6.4C19.088 6.4 21.6 8.912 21.6 12C21.6 15.088 19.088 17.6 16 17.6C12.912 17.6 10.4 15.088 10.4 12C10.4 8.912 12.912 6.4 16 6.4ZM16 28.8C12.752 28.8 8.912 27.488 6.176 24.192C8.97857 21.9932 12.4378 20.7981 16 20.7981C19.5622 20.7981 23.0214 21.9932 25.824 24.192C23.088 27.488 19.248 28.8 16 28.8Z"
+                                                        fill="#FFFFFF"
+                                                    />
+                                                </svg>
+                                            </NavLink>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </nav>
+                        </div>
+                    </Modal>
+                </>
             )}
         </>
     );
